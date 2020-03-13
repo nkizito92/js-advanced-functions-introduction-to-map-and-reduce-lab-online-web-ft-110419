ca0419c3c5f584cbf5243a9fcff65c8a0ad0cc41
Kizito Njoku <nkizito57@gmail.com>
2020-03-13 15:50:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+  let newArr = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArr.push(sourceArray[i] * -1)
+  }
+    return newArr
+}
+
+function mapToNoChange(sourceArray) {
+  let newArr = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArr.push(sourceArray[i])
+  }
+  return newArr
+}
+
+function mapToDouble(sourceArray) {
+    let newArr = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArr.push(sourceArray[i] * 2)
+  }
+  return newArr
+}
+
+function mapToSquare(sourceArray) {
+    let newArr = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArr.push(sourceArray[i]**2)
+  }
+  return newArr
+}
+
+function reduceToTotal(sourceArray, startingPoint){
+  
+}
