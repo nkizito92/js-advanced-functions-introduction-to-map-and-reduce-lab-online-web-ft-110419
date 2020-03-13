c2ffef7161b5bf06abd89abb58b77ab7572d28f3
Kizito Njoku <nkizito57@gmail.com>
2020-03-13 18:01:07 +0000

    Done.

--- index.js
+++ index.js
@@ -32,6 +32,26 @@ function mapToSquare(sourceArray) {
   return newArr
 }
 
-function reduceToTotal(sourceArray, startingPoint){
-  
+function reduceToTotal(sourceArray, startingPoint=0){
+  for(let i = 0; i < sourceArray.length; i++) {
+    startingPoint += sourceArray[i] 
+  }
+  return startingPoint
+}
+
+function reduceToAllTrue(sourceArray) {
+
+for (let i = 0; i < sourceArray.length; i++ ) {
+    if (!sourceArray[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+   for (let i = 0; i < sourceArray.length; i++ ) {
+    if (sourceArray[i]) {
+       return true
+    } 
+  }
+  return false
 }
